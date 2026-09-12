368cfab131503bc00c28641c6d2f930fe335876e
Quentin LEGER <quentin.leger1@etu.univ-lorraine.fr>
2026-09-12 14:54:49 +0200

    Ajout de GROQ API

--- backend2/agents/main.py
+++ backend2/agents/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from audio import speak_async
 import json
-from nova import nova
+from nova import ask_nova
 
 import requests
 
@@ -21,26 +21,23 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             payload = json.loads(data)  # transforme la string JSON en dict Python
             texte = payload.get("user_message", "")
-            voice_target = payload.gget("voice_target", "serveur")
+            voice_target = payload.get("voice_target", "serveur")
 
-            response_text = "Message recu"
-
-            if texte and "nova" in texte:
+            if texte and "nova" in texte.lower():
                 commande = texte.lower().replace("nova", "").strip()
 
-                if voice_target == "navigateur":
-                    await websocket.send_json({"type": "speak_local", "text": commande})
 
-                elif voice_target == "serveur":
-                    await websocket.send_json({"type": "speech", "text": "Message reçu !"})
-                    await speak_async(texte)
+                result = ask_nova(commande)
+                if voice_target == "serveur":
+                    await websocket.send_json({"type": "speech", "text": result})
+                    await speak_async(result)
 
                 else :
-                    await websocket.send_json({"type": "text", "text": commande})
+                    await websocket.send_json({"type": "text", "text": result})
 
                     url = f"http://{get_ip.get(voice_target)}/speak"
                     try :
-                        requests.post(url,json={"text":commande},timeout=5)
+                        requests.post(url,json={"text":result},timeout=5)
                     except Exception as e:
                         print(f"Agent injoignable : {e}")
 
--- backend2/agents/nova.py
+++ backend2/agents/nova.py
@@ -1,21 +1,17 @@
 from groq import Groq
 from dotenv import load_dotenv
-import json
-import requests
 import os
 
 load_dotenv()
 api_key = os.getenv("api_key")
 client = Groq(api_key=api_key)
 
-
 def ask_nova(message: str):
-    prompt =f"{message}"
-
+    prompt = f"{message}"
 
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="openai/gpt-oss-20b",  # Copié direct de ta liste API
         messages=[{"role": "user", "content": prompt}]
     )
 
-    print(response)
+    return response.choices[0].message.content
