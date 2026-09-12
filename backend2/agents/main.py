f859d29cbbc8cc2840ebd767c8592665b1c8b426
Quentin LEGER <quentin.leger1@etu.univ-lorraine.fr>
2026-09-12 11:43:59 +0200

    Choix voix et IP

--- backend2/agents/main.py
+++ backend2/agents/main.py
@@ -2,8 +2,14 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from audio import speak_async
 import json
 
+import requests
+
 app =FastAPI()
 
+
+get_ip = {
+    "pc_fixe": "192.168.1.18:5001",
+}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -14,9 +20,27 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             payload = json.loads(data)  # transforme la string JSON en dict Python
             texte = payload.get("user_message", "")
+            voice_target = payload.gget("voice_target", "serveur")
 
-            await websocket.send_json({"type": "speech", "text": "Message reçu !"})
-            await speak_async(texte)
+            response_text = "Message recu"
+
+            if voice_target == "navigateur":
+                await websocket.send_json({"type": "speak_local", "text": response_text})
+
+            elif voice_target == "serveur":
+                await websocket.send_json({"type": "speech", "text": "Message reçu !"})
+                await speak_async(texte)
+
+            else :
+                await websocket.send_json({"type": "text", "text": response_text})
+
+                url = f"http://{get_ip.get(voice_target)}/speak"
+                try :
+                    requests.post(url,json={"text":response_text},timeout=5)
+                except Exception as e:
+                    print(f"Agent injoignable : {e}")
+
+
     except WebSocketDisconnect:
         print("WebSocket deconnecté")
 
